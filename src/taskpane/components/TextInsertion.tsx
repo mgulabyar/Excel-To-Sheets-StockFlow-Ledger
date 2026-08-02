27ad7a4585c2d0e64d835180e81e1be2129516e6
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-08-02 14:40:13 +0500

    feat: implement two-step approval workflow draft-pending-approved

--- src/taskpane/components/TextInsertion.tsx
+++ src/taskpane/components/TextInsertion.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useState } from "react";
 import { Button, Field, Textarea, tokens, makeStyles } from "@fluentui/react-components";
 
-/* global HTMLTextAreaElement */
+/* global  */
 
 interface TextInsertionProps {
   insertText: (text: string) => void;
