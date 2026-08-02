c910000f93eccbe9c6424ec8da7f7988053e6dce
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-08-02 14:40:28 +0500

    feat: add ApprovalPanel UI component for finance approval

--- src/taskpane/components/TextInsertion.tsx
+++ src/taskpane/components/TextInsertion.tsx
@@ -2,8 +2,6 @@ import * as React from "react";
 import { useState } from "react";
 import { Button, Field, Textarea, tokens, makeStyles } from "@fluentui/react-components";
 
-/* global  */
-
 interface TextInsertionProps {
   insertText: (text: string) => void;
 }
