5eeaa3c8a4fa174e06cff265bc8b09e32d5b5b48
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-08-03 19:25:44 +0500

    Refactor push flow to validate duplicates before API call

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -681,7 +681,7 @@ export default function App() {
           },
         }}
       />
-{/* quantitity input */}
+{/* quantitity */}
       <TextField
         label="Quantity"
         type="number"
