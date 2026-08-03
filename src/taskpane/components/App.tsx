e9cedf80e4f99370ea7140fb95bbc8ca0ac76520
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-08-03 22:53:29 +0500

    Block duplicate inventory writes with error toast

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -340,18 +340,6 @@
 //           },
 //         }}
 //       >
-//         {loading ? <CircularProgress size={18} color="inherit" sx={{ mr: 1 }} /> : "Send to Sheets"}
-//       </Button>
-
-//       <Button
-//         variant="outlined"
-//         disabled={loading}
-//         onClick={() => fetchFromSheets(false)}
-//         fullWidth
-//         startIcon={<RefreshIcon sx={{ fontSize: "16px" }} />}
-//         sx={{
-//           width: "90%",
-
 
 
 declare const Excel: any;
